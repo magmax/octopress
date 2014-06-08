753402d4083716a13b7e148b8d94eff30b4132df
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-06-08 17:14:33 +0200

    Adding tags and fixing 404 errors

--- source/404page.php
+++ source/404page.php
@@ -8,9 +8,12 @@
 
    $url_map = array(
        '/2014/02/22/lamp-con-salt.html' => '/blog/2014/01/22/lamp-con-salt/',
+       '/blog/2014/02/22/lamp-con-salt' => '/blog/2014/01/22/lamp-con-salt/',
+       '/archive.html' => '/blog/archives',
+       '/blog/2011/08/13/apache-derby' => '/blog/2011/04/23/apache-derby/',
    );
-   if ( in_array($_SERVER["REQUEST_URI"], $url_map)) {
-       redirect($url_map[$_SERVER["REQUEST_URI"]]);
+   if ( array_key_exists($_SERVER["REQUEST_URI"], $url_map)) {
+       redirect($url_map[$_SERVER["REQUEST_URI"]], true);
    }
 
 
@@ -18,7 +21,17 @@
        redirect('/blog' . $matches[1], true);
    }
    if ( preg_match("@^/page(\d+)@", $_SERVER["REQUEST_URI"], $matches) ) {
-       redirect('/page/' . $matches[1], false);
+       redirect('/blog/page/' . $matches[1], true);
    }
+   if ( preg_match("@^/blog/blog/(.*)$@", $_SERVER["REQUEST_URI"], $matches) ) {
+       redirect('/blog/' . $matches[1], true);
+   }
+   if ( preg_match("@^(/blog/.*).html$@", $_SERVER["REQUEST_URI"], $matches) ) {
+       redirect($matches[1], true);
+   }
+   if ( preg_match("@^/drupal/.*@", $_SERVER["REQUEST_URI"], $matches) ) {
+       redirect('/', true);
+   }
+
    redirect('/404page.html');
 ?>
